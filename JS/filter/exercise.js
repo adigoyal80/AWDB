0d29e5fb5137d0f383e07cc355432006e3664553
Aditya Goyal <aditya.goyal@coatesdigital.com.au>
2018-04-18 16:45:01 +1000

    filter exercise

---
+++ JS/filter/exercise.js
@@ -0,0 +1,83 @@
+/*
+Write a function called filterByValue which accepts an array of objects and a key and returns a new array with all the objects that contain that key.
+
+Examples:
+    filterByValue([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner') // [{first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Colt', last:"Steele", isCatOwner: true}]
+*/
+
+function filterByValue(arr, key){
+    return arr.filter((el) => {
+        if(key in el){
+            return el
+        }
+    })
+}
+// let test = filterByValue([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner')
+// console.log(test)
+/*
+Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
+
+Examples:
+    find([1,2,3,4,5], 3) // 3
+    find([1,2,3,4,5], 10) // undefined
+*/
+
+function find(arr, searchValue){
+    let result = arr.filter((el) => {
+        return el === searchValue;
+    })
+    return result.length === 0 ? undefined : result
+}
+
+/*
+Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the arrayt.
+
+Examples:
+    findInObj([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true) // {first: 'Tim', last:"Garcia", isCatOwner: true}
+*/
+
+function findInObj(arr, key, searchValue){
+    let found = arr.filter((el) => {
+        return key in el && el[key] === searchValue
+    })
+    return found.length === 0 ? undefined : found[0];
+}
+
+
+/*
+Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
+
+Examples:
+    removeVowels('Elie') // ('l')
+    removeVowels('TIM') // ('tm')
+    removeVowels('ZZZZZZ') // ('zzzzzz')
+*/
+
+function removeVowels(str){
+    let vowels = 'aeiou';
+    let newStr = '';
+    Array.from(str).filter((e) => {
+        e = e.toLowerCase()
+        if(vowels.indexOf(e) === -1){
+            newStr += e;
+        }
+    })
+    return newStr;
+}
+
+
+/*
+Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and fitler to double and then filter the odd numbers).
+
+Examples:
+    doubleOddNumbers([1,2,3,4,5]) // [2,6,10]
+    doubleOddNumbers([4,4,4,4,4]) // []
+*/
+
+function doubleOddNumbers(arr){
+    return arr.filter((el) => {
+        return el % 2 != 0;
+    }).map((el) => {
+        return el * 2; 
+    })
+}
