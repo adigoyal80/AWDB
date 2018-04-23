712459f5944a199a4d406ee7c3a0d1fcca4433f7
Aditya Goyal <aditya.goyal@coatesdigital.com.au>
2018-04-23 13:12:38 +1000

    oop progress

---
+++ JS/OOP/vehicles.js
@@ -0,0 +1,13 @@
+function Car(make, model, year){
+    this.make = make;;
+    this.model = model;
+    this.year = year;
+    this.numWheels = 4;
+}
+
+function Motorcycle(make, mode, year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.numWheels = 2;
+}
